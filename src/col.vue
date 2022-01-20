a9666e0f9f1905ac1ecfa89a942aeb95d2e3df18
xu <chenhengxv@outlook.com>
2022-01-20 17:29:30 +0800

    初步实现响应式 1

--- src/col.vue
+++ src/col.vue
@@ -14,6 +14,20 @@ export default {
     offset: {
       type: [Number, String]
     },
+    phone: {
+      type: Object,
+      validator(value) {
+        keys = Object.keys(value)
+        let keys = Object.keys(value)
+        let valid = true
+        keys.forEach(key => {
+          if (!["span", "offset"].includes(key)) {
+            valid = false
+          }
+        })
+        return valid
+      }
+    }
   },
   data() {
     return {
@@ -23,10 +37,15 @@ export default {
   },
   computed: {
     colClass() {
-      let {span, offset} = this
+      let {span, offset,phone} = this
+      let phoneClass =[]
+      if (phone){
+       phoneClass = [`col-phone-${phone.span}`]
+      }
       return [
-          span && `col-${span}`,
+        span && `col-${span}`,
         offset && `offset-${offset}`,
+        ...phoneClass
       ]
     },
     colStyle() {
@@ -56,4 +75,32 @@ export default {
     }
   }
 }
-</style>
+
+@media (max-width: 576px) {
+  //$class-prefix: col-phone-;
+  //@for $n from 1 through 24 {
+  //  &.#{$class-prefix}#{$n} {
+  //    width: ($n / 24) * 100%;
+  //  }
+  //}
+  //$class-prefix: offset-phone-;
+  //@for $n from 1 through 24 {
+  //  &.#{$class-prefix}#{$n} {
+  //    margin-left: ($n / 24) * 100%;
+  //  }
+  //}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
