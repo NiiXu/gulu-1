0b97959a1e9ddc65497a237bc8c5fcc379084eb5
xu <chenhengxv@outlook.com>
2022-01-24 17:05:11 +0800

    tab 切换动画完成

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="tabs-head">
     <slot></slot>
+    <div class="line" ref="line"></div>
     <div class="actions-wrapper">
       <slot name="actions"></slot>
     </div>
@@ -11,18 +12,31 @@
 export default {
   name: 'GuluTabsHead',
   inject: ['eventBus'],
-  created () {
+  mounted () {
+    this.eventBus.$on('update:selected', (item, vm) => {
+      let {width, height, top, left} = vm.$el.getBoundingClientRect()
+      this.$refs.line.style.width = `${width}px`
+      this.$refs.line.style.left = `${left}px`
+    })
   }
 }
 </script>
 
 <style scoped lang="scss">
 $tab-height: 40px;
+$blue: blue;
 .tabs-head {
   display: flex;
   height: $tab-height;
   justify-content: flex-start;
   border: 1px solid red;
+  position: relative;
+  > .line {
+    position: absolute;
+    bottom: 0;
+    border-bottom: 1px solid $blue;
+    transition: all 350ms;
+  }
   > .actions-wrapper {
     margin-left: auto;
   }
--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -37,7 +37,7 @@ export default {
   },
   methods: {
     xxx() {
-      this.eventBus.$emit('update:selected', this.name)
+      this.eventBus.$emit('update:selected', this.name, this)
     }
   }
 }
@@ -45,16 +45,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$blue: blue;
 .tabs-item {
   flex-shrink: 0;
   padding: 0 1em;
   cursor: pointer;
-  border: 1px solid green;
   height: 100%;
   display: flex;
   align-items: center;
   &.active {
-    background: red;
+    color: $blue;
+    font-weight: bold;
   }
 }
 </style>
