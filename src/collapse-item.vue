dfa59470b90ce9d611b6e82a9ae6a683be37ce29
xu <chenhengxv@outlook.com>
2022-01-26 20:29:54 +0800

    可以设置默认 selected

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="collapseItem">
     <div class="title" @click="toggle">
-      {{title}}
+      {{ title }}
     </div>
     <div class="content" v-if="open">
       <slot></slot>
@@ -16,32 +16,40 @@ export default {
     title: {
       type: String,
       required: true
-    }
-  },
-  data () {
-    return {
-      open: false
-    }
-  },
-  inject: ['eventBus'],
-  mounted () {
-    this.eventBus && this.eventBus.$on('update:selected', (vm) => {
-      if (vm !== this) {
-        this.close()
-      }
-    })
-  },
-  methods: {
-    toggle () {
-      if (this.open) {
-        this.open = false
-      } else {
-        this.open = true
-        this.eventBus && this.eventBus.$emit('update:selected', this)
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    data() {
+      return {
+        open: false
       }
     },
-    close () {
-      this.open = false
+    inject: ["eventBus"],
+    mounted() {
+      this.eventBus && this.eventBus.$on("update:selected", (name) => {
+        if (name !== this.name) {
+          this.close()
+        } else {
+          this.show()
+        }
+      })
+    },
+    methods: {
+      toggle() {
+        if (this.open) {
+          this.open = false
+        } else {
+          this.eventBus && this.eventBus.$emit("update:selected", this.name)
+        }
+      },
+      close() {
+        this.open = false
+      }
+    },
+    show() {
+      this.open = true
     }
   },
 }
@@ -61,18 +69,21 @@ $border-radius: 4px;
     align-items: center;
     padding: 0 8px;
   }
+
   &:first-child {
     > .title {
       border-top-left-radius: $border-radius;
       border-top-right-radius: $border-radius;
     }
   }
+
   &:last-child {
     > .title:last-child {
       border-bottom-left-radius: $border-radius;
       border-bottom-right-radius: $border-radius;
     }
   }
+
   > .content {
     padding: 8px;
   }
--- src/collapse.vue
+++ src/collapse.vue
@@ -5,25 +5,33 @@
 </template>
 
 <script>
-import Vue from 'vue'
+import Vue from "vue"
+
 export default {
   name: "GuluCollapse",
   props: {
     single: {
       type: Boolean,
       default: false
-    }
-  },
-  data () {
-    return {
-      eventBus: new Vue()
-    }
-  },
-  provide () {
-    if (this.single) {
+    },
+    selected: {
+      type: String,
+    },
+    data() {
+      return {
+        eventBus: new Vue()
+      }
+    },
+    provide() {
       return {
         eventBus: this.eventBus
       }
+    },
+    mounted() {
+      this.eventBus.$emit("update:selected", this.selected)
+      this.eventBus.$on("update:selected", (name) => {
+        this.$emit("update:selected", name)
+      })
     }
   }
 }
